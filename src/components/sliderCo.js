eb122046c5f62fecdd40af9147c1afc79e31b99e
johngom <zemog1916@gmail.com>
2020-05-29 12:09:58 -0500

    some styles

--- src/components/sliderCo.js
+++ src/components/sliderCo.js
@@ -1,21 +1,30 @@
 import React from "react"
 import Slider from "@material-ui/core/Slider"
 import InputLabel from "@material-ui/core/InputLabel"
-// import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import PropTypes from "prop-types"
 
 import TooltipCO from "./tooltipCo"
 
-// const useStyles = makeStyles(() => ({}))
+const useStyles = makeStyles((theme) => ({
+  root: (props) => ({
+    width: props.width || "11.25rem",
+  }),
+  label: {
+    fontSize: "0.875rem",
+    marginBottom: "1rem",
+    color: theme.palette.grey[700],
+  },
+}))
 
-const SliderCo = ({ value, handleChange, label, ...props }) => {
-  // const classes = useStyles()
+const SliderCo = ({ value, styles, handleChange, label, ...props }) => {
+  const classes = useStyles(styles)
   const onChange = (event, newValue) => {
     handleChange(newValue)
   }
   return (
-    <div>
-      {label && <InputLabel>{label}</InputLabel>}
+    <div className={classes.root}>
+      {label && <InputLabel className={classes.label}>{label}</InputLabel>}
       <Slider
         {...props}
         value={value}
@@ -31,6 +40,7 @@ SliderCo.propTypes = {
   max: PropTypes.number,
   step: PropTypes.number,
   label: PropTypes.string,
+  styles: PropTypes.shape,
   value: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.arrayOf(PropTypes.number),
@@ -44,6 +54,7 @@ SliderCo.defaultProps = {
   step: 1,
   label: "",
   value: 10,
+  styles: {},
 }
 
 export default SliderCo
--- src/section/SliderSection.js
+++ src/section/SliderSection.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react"
-import Container from "@material-ui/core/Container"
+import { makeStyles } from "@material-ui/core/styles"
 
 import SliderCo from "../components/sliderCo"
 
+const useStyles = makeStyles(() => ({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 50,
+  },
+}))
+
 const SliderSection = () => {
+  const classes = useStyles()
   const [slide, setSlide] = useState(20)
   const [slideDouble, setSlideDouble] = useState([10, 50])
 
   return (
-    <Container
-      maxWidth="xs"
-      style={{
-        marginBottom: 50,
-      }}
-    >
+    <div className={classes.container}>
       <SliderCo
         value={slide}
         scale={(x) => `${x} USD`}
         handleChange={setSlide}
         label="Un Nodo"
+        min={1000}
+        max={100000}
+        step={1000}
       />
       <SliderCo
         value={slideDouble}
@@ -33,7 +41,7 @@ const SliderSection = () => {
         handleChange={setSlide}
         label="Dos Nodos"
       />
-    </Container>
+    </div>
   )
 }
 
