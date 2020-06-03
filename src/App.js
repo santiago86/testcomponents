6c1a10d5ca9475254f42f32f2562dc0c4834104a
sebastian tobon <setohe.09@gmail.com>
2020-06-02 23:57:28 -0500

    alertComponent created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Container from "@material-ui/core/Container"
 import Switch from "@material-ui/core/Switch"
 
@@ -14,9 +14,10 @@ import SwichtSection from "./section/SwichtSection"
 
 import SliderSection from "./section/SliderSection"
 import RadioButton from "./components/RadioButton/Form"
+import AlertSection from "./section/AlertSection"
 
 function App() {
-  const [check, setCheck] = React.useState(false)
+  const [check, setCheck] = useState(false)
   return (
     <div className="App">
       <Container max-width="lg">
@@ -50,6 +51,8 @@ function App() {
           <SliderSection />
           <h2> Radio Button </h2>
           <RadioButton />
+          <h2> Alert Message </h2>
+          <AlertSection />
         </header>
       </Container>
     </div>
