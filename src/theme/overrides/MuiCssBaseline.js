240a13edd11879fe02970c1443d200891a16b1fd
handfulofcats <andrescanelones10@gmail.com>
2020-06-08 15:24:03 -0500

    Font Family Updates

--- src/theme/overrides/MuiCssBaseline.js
+++ src/theme/overrides/MuiCssBaseline.js
@@ -1,7 +1,23 @@
-import SuisseIntlLight from "../../assets/fonts/SuisseIntl-Light-WebS.woff2"
-import SuisseIntlRegular from "../../assets/fonts/SuisseIntl-Regular-WebS.woff2"
-import SuisseIntlMedium from "../../assets/fonts/SuisseIntl-Medium-WebS.woff2"
-import SuisseIntlSemibold from "../../assets/fonts/SuisseIntl-SemiBold-WebS.woff2"
+import SuisseIntlLightWoff2 from "../../assets/fonts/SuisseIntl-Light.woff2"
+import SuisseIntlRegularWoff2 from "../../assets/fonts/SuisseIntl-Regular.woff2"
+import SuisseIntlMediumWoff2 from "../../assets/fonts/SuisseIntl-Medium.woff2"
+import SuisseIntlSemiboldWoff2 from "../../assets/fonts/SuisseIntl-SemiBold.woff2"
+
+import SuisseIntlLightWoff from "../../assets/fonts/SuisseIntl-Light.woff"
+import SuisseIntlRegularWoff from "../../assets/fonts/SuisseIntl-Regular.woff"
+import SuisseIntlMediumWoff from "../../assets/fonts/SuisseIntl-Medium.woff"
+import SuisseIntlSemiboldWoff from "../../assets/fonts/SuisseIntl-SemiBold.woff"
+
+import SuisseIntlLightTTF from "../../assets/fonts/SuisseIntl-Light.ttf"
+import SuisseIntlRegularTTF from "../../assets/fonts/SuisseIntl-Regular.ttf"
+import SuisseIntlMediumTTF from "../../assets/fonts/SuisseIntl-Medium.ttf"
+import SuisseIntlSemiboldTTF from "../../assets/fonts/SuisseIntl-SemiBold.ttf"
+
+import SuisseIntlLightEOT from "../../assets/fonts/SuisseIntl-Light.eot"
+import SuisseIntlRegularEOT from "../../assets/fonts/SuisseIntl-Regular.eot"
+import SuisseIntlMediumEOT from "../../assets/fonts/SuisseIntl-Medium.eot"
+import SuisseIntlSemiboldEOT from "../../assets/fonts/SuisseIntl-SemiBold.eot"
+
 import palette from "../palette"
 
 const suisseLight = {
@@ -9,11 +25,7 @@ const suisseLight = {
   fontStyle: "normal",
   fontDisplay: "swap",
   fontWeight: 300,
-  src: `
-    url(${SuisseIntlLight}) format('woff2')
-  `,
-  unicodeRange:
-    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+  src: `url('${SuisseIntlLightEOT}?#iefix') format('embedded-opentype'), url('${SuisseIntlLightWoff2}') format('woff2'), url('${SuisseIntlLightWoff}') format('woff'), url('${SuisseIntlLightTTF}') format('truetype')`,
 }
 
 const suisseRegular = {
@@ -21,11 +33,7 @@ const suisseRegular = {
   fontStyle: "normal",
   fontDisplay: "swap",
   fontWeight: 400,
-  src: `
-    url(${SuisseIntlRegular}) format('woff2')
-  `,
-  unicodeRange:
-    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+  src: `url('${SuisseIntlRegularEOT}?#iefix') format('embedded-opentype'), url('${SuisseIntlRegularWoff2}') format('woff2'), url('${SuisseIntlRegularWoff}') format('woff'), url('${SuisseIntlRegularTTF}') format('truetype')`,
 }
 
 const suisseMedium = {
@@ -33,11 +41,7 @@ const suisseMedium = {
   fontStyle: "normal",
   fontDisplay: "swap",
   fontWeight: 500,
-  src: `
-    url(${SuisseIntlMedium}) format('woff2')
-  `,
-  unicodeRange:
-    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+  src: `url('${SuisseIntlMediumEOT}?#iefix') format('embedded-opentype'), url('${SuisseIntlMediumWoff2}') format('woff2'), url('${SuisseIntlMediumWoff}') format('woff'), url('${SuisseIntlMediumTTF}') format('truetype')`,
 }
 
 const suisseSemibold = {
@@ -45,11 +49,7 @@ const suisseSemibold = {
   fontStyle: "normal",
   fontDisplay: "swap",
   fontWeight: 600,
-  src: `
-    url(${SuisseIntlSemibold}) format('woff2')
-  `,
-  unicodeRange:
-    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+  src: `url('${SuisseIntlSemiboldEOT}?#iefix') format('embedded-opentype'), url('${SuisseIntlSemiboldWoff2}') format('woff2'), url('${SuisseIntlSemiboldWoff}') format('woff'), url('${SuisseIntlSemiboldTTF}') format('truetype')`,
 }
 
 export default {
